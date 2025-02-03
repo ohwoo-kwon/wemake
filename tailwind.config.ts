308b23d56dfba3dbaa76daae88eb65058505b567
ohwoo-kwon <kwonow1@gmail.com>
2025-02-03 09:34:11 +0900

    1.0 Create Project

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+export default {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
