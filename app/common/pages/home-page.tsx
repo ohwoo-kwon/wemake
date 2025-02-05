943500195654166769c131d709832648a516b27b
ohwoo-kwon <kwonow1@gmail.com>
2025-02-05 21:09:57 +0900

    #3.5 Product Card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,112 +1,36 @@
+import { type MetaFunction } from "react-router";
+import ProductCard from "~/features/products/components/product-card";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Home | wemake" },
+    { name: "description", content: "Welcome to wemake" },
+  ];
+};
+
 export default function HomePage() {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-      ratione, aut unde nulla magni eaque sunt incidunt a iste earum odit dolore
-      hic vitae obcaecati! Voluptas culpa, vero molestias aspernatur ab quo
-      fugiat! Incidunt consectetur iusto aliquid omnis culpa nam, optio, ipsum
-      sapiente amet, eos delectus doloribus blanditiis deserunt? Qui quisquam
-      eius, assumenda, distinctio eaque tempore necessitatibus aliquid nam
-      maxime molestias error voluptas veritatis, eveniet dicta quam
-      reprehenderit? Nulla nobis labore, aliquam neque optio, possimus ad odit
-      numquam voluptas est perferendis quas doloremque ab. Non corporis
-      molestias placeat deleniti eaque. Eveniet quibusdam sunt impedit magni
-      nemo aspernatur ab nobis inventore nostrum nihil, aliquam aliquid
-      temporibus. Laborum, magni aut earum quam quo vero at ducimus similique
-      numquam possimus veniam consequatur laboriosam consectetur assumenda
-      quidem atque unde, saepe quisquam necessitatibus recusandae. Dolores odit
-      perspiciatis accusantium? Perspiciatis porro eligendi facere minima saepe,
-      labore, repudiandae atque doloremque incidunt, blanditiis voluptatibus
-      eveniet veniam sequi nulla! Non consequuntur quam cum doloribus, modi quae
-      laboriosam culpa numquam veniam et eos asperiores ratione, perferendis
-      aliquam vel, in blanditiis eum nesciunt? Consequuntur tenetur doloribus
-      obcaecati vel quod. Dicta, dolore! Molestiae aperiam perspiciatis labore
-      eum, eos libero consequatur iure incidunt eaque ipsum sed iste cumque
-      voluptatum quis aliquam ipsa mollitia quo sit ullam quos tenetur hic
-      consequuntur? Similique repellat blanditiis quod molestias cumque iure,
-      amet itaque dolorem dolorum tenetur cupiditate maxime consequuntur facilis
-      eligendi, sit, placeat vitae. Amet veritatis consequatur harum provident
-      et officia voluptatem eius laborum mollitia odio cupiditate natus aliquid,
-      eaque fugit ad soluta labore aut dolorum quis, id veniam dolorem
-      dignissimos! Voluptate sed nisi explicabo amet distinctio iusto
-      laudantium, numquam, quasi quae sunt aut quod voluptatem nihil?
-      Dignissimos sapiente, et tempore deleniti ab aperiam maiores minima
-      recusandae soluta consectetur porro, at laborum nisi consequatur atque
-      molestias dolor veniam ad? Quo quod laborum velit tenetur perspiciatis.
-      Delectus, sunt amet officia dolorum fuga accusamus, veritatis totam, iusto
-      laboriosam deserunt magni? Sequi error commodi ipsam voluptate, sapiente
-      ea beatae soluta similique iusto ullam eaque voluptatum. Porro quis harum
-      veniam. Consequuntur illum reprehenderit officiis facere, officia hic
-      voluptatum vitae nesciunt quidem, cumque ut eos iusto alias harum
-      perspiciatis rem magnam corporis porro ullam molestiae molestias. Corrupti
-      facilis veritatis qui obcaecati consectetur at eveniet labore deleniti ea
-      esse repudiandae a placeat fugiat ex vel nam modi, dolor cupiditate
-      dolorem! Omnis id alias distinctio explicabo repellat reprehenderit
-      tempora repudiandae sequi enim ut natus at aliquid unde nesciunt quod nisi
-      provident eaque minus est delectus numquam, laboriosam iste sit pariatur.
-      Enim obcaecati rerum rem eligendi unde error aliquid, dolore veritatis.
-      Excepturi porro animi tempora voluptatum! Sint quam perspiciatis earum et,
-      facilis minima sequi repellendus ea consequatur nemo veniam modi enim
-      officia aperiam incidunt esse dignissimos nostrum commodi quo unde? Eaque
-      ipsa totam voluptas vero laudantium, est a corporis quisquam ex enim quo
-      magni consequuntur commodi eius minus laboriosam molestias eligendi natus
-      facilis aliquam modi. Voluptate ipsum odio est aut consequuntur commodi
-      voluptates rem ab aliquid fuga, reprehenderit vitae. Laudantium possimus
-      placeat molestias perferendis natus deleniti consequuntur nesciunt.
-      Cupiditate atque fugiat qui odit error praesentium dolores distinctio
-      dolorum quasi ab inventore illum porro dolor, maiores alias ipsa molestias
-      aperiam. Voluptates nostrum ducimus quos esse pariatur adipisci quod
-      corrupti provident eaque quidem enim veniam voluptatem aliquam repellat
-      quia vero excepturi corporis quibusdam suscipit error obcaecati,
-      repellendus, blanditiis impedit. Enim illum quisquam harum quos? Provident
-      sunt placeat quibusdam laborum doloribus a nulla, exercitationem quia
-      dolor cupiditate ex vel maiores eaque eius iste fuga voluptates ducimus
-      eligendi fugit impedit asperiores nihil? Itaque eos iste fuga unde, facere
-      laudantium natus. Delectus dolor, sint excepturi suscipit eligendi
-      reiciendis rerum cupiditate laboriosam voluptatum vero optio consequuntur
-      voluptates rem perspiciatis earum voluptatibus velit officia tenetur dicta
-      architecto! Repudiandae nobis quod eum corrupti modi reprehenderit facere
-      error tenetur doloremque quos perspiciatis maiores laborum, illo et
-      molestias rem sint ab optio maxime itaque similique ipsa! Nihil at,
-      architecto expedita nobis placeat neque error, ex veritatis officia beatae
-      quidem impedit deleniti doloremque iste aut vero aliquam illum quasi,
-      tenetur suscipit dolores ab corrupti autem voluptatem. Accusamus modi quod
-      ipsa voluptatibus illum cupiditate ratione quae, minus error non molestias
-      eveniet possimus, eum magni? Fugit esse, cum voluptatem ipsam in sint
-      laudantium minima aspernatur facere reprehenderit eligendi, voluptates
-      perferendis velit, animi ut omnis. Saepe distinctio numquam consequuntur
-      aliquid quis cupiditate eius tempore accusamus asperiores quaerat maiores,
-      doloribus ipsa veniam amet quo vitae voluptatibus, aut doloremque! Nulla
-      nihil nobis dolore inventore est. Magni accusamus earum autem quaerat a
-      perferendis sapiente laudantium nam repellendus maiores sit cum enim iure
-      commodi corrupti consequuntur similique facere aliquam quo eos, ab dolores
-      distinctio? Ex beatae officiis esse deserunt explicabo, quidem culpa
-      sapiente quas eius soluta sequi aspernatur quis corporis, id neque hic
-      sint laborum atque, illo magnam. A sunt dolores quibusdam delectus ipsam
-      sit animi quas odio, optio omnis dolore nihil praesentium. Suscipit quidem
-      quasi placeat voluptas, alias minima cum, aliquid quis sunt laboriosam
-      fugit et omnis ex illo vero! Vero aut impedit consequatur aspernatur
-      tenetur ad! Corrupti enim voluptatem provident suscipit ab alias ratione
-      rerum beatae impedit similique minima sunt adipisci accusantium et
-      repellat, ullam exercitationem voluptatibus perferendis odit tempora
-      veniam cumque veritatis. Deleniti sapiente recusandae quos, fugit
-      reiciendis similique consectetur temporibus, nisi blanditiis officia
-      dolorum! Natus quae dolorum possimus odit libero expedita voluptates illo
-      minus? Accusamus asperiores aliquam, itaque fuga ipsa perferendis at,
-      sequi mollitia esse optio vitae id similique quas nisi expedita iste
-      consectetur? Itaque pariatur error cum animi omnis, odio vitae maxime ex,
-      neque quas corporis totam, necessitatibus consequuntur magni eaque
-      veritatis. Sed aperiam, obcaecati qui dolor necessitatibus ab impedit.
-      Repudiandae aspernatur, dolorum maxime vero nihil perspiciatis voluptatem
-      excepturi a labore earum pariatur nulla eum recusandae fugit officiis
-      beatae laborum nisi enim sunt, provident ipsum delectus minus,
-      necessitatibus est. Nihil possimus dicta eveniet aperiam sed id earum iste
-      similique? Quam maxime repellendus nemo minima quo explicabo dignissimos,
-      velit aliquam error cum quas libero dolore sequi, ducimus adipisci
-      dolorum, consequuntur provident commodi sit praesentium? Deleniti beatae
-      officia voluptate necessitatibus voluptas hic aperiam quaerat quibusdam
-      pariatur id. Aliquid voluptas, vel at doloremque saepe praesentium,
-      reiciendis, adipisci deleniti commodi aperiam corporis!
+    <div className="px-20">
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Today's Products
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The best products made by our community today.
+          </p>
+        </div>
+        {Array.from({ length: 10 }).map((_, idx) => (
+          <ProductCard
+            id={`productId-${idx}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={41}
+            votesCount={120}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ app/features/products/components/product-card.tsx
@@ -0,0 +1,59 @@
+import { ChevronUpIcon, EyeIcon, MessageCircleIcon } from "lucide-react";
+import { Link } from "react-router";
+import { Button } from "~/common/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+
+interface ProductCardProps {
+  id: string;
+  name: string;
+  description: string;
+  commentsCount: number;
+  viewsCount: number;
+  votesCount: number;
+}
+
+export default function ProductCard({
+  id,
+  name,
+  description,
+  commentsCount,
+  viewsCount,
+  votesCount,
+}: ProductCardProps) {
+  return (
+    <Link to={`/products/${id}`}>
+      <Card className="w-full flex items-center justify-between bg-transparent hover:bg-card/50">
+        <CardHeader>
+          <CardTitle className="text-2xl font-semibold leading-none tracking-tight">
+            {name}
+          </CardTitle>
+          <CardDescription className="text-muted-foreground">
+            {description}
+          </CardDescription>
+          <div className="flex items-center gap-4 mt-2">
+            <div className="flex items-center gap-px text-sm text-muted-foreground">
+              <MessageCircleIcon className="size-4" />
+              <span>{commentsCount}</span>
+            </div>
+            <div className="flex items-center gap-px text-sm text-muted-foreground">
+              <EyeIcon className="size-4" />
+              <span>{viewsCount}</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter className="py-0">
+          <Button variant="outline" className="flex flex-col h-14">
+            <ChevronUpIcon className="size-4 shrink-0" />
+            <span>{votesCount}</span>
+          </Button>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+}
