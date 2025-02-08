e767d76c8c3aa35d4038ee7893054f12a0be78fc
ohwoo-kwon <kwonow1@gmail.com>
2025-02-08 11:29:49 +0900

    #3.15 Data Validation

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,3 +1,58 @@
-export default function DailyLeaderboard() {
+import { DateTime } from "luxon";
+import type { Route } from "./+types/daily-leaderboard-page";
+import { data, isRouteErrorResponse } from "react-router";
+import { z } from "zod";
+
+const paramsSchema = z.object({
+  year: z.coerce.number(),
+  month: z.coerce.number(),
+  day: z.coerce.number(),
+});
+
+export const loader = ({ params }: Route.LoaderArgs) => {
+  const { success, data: parsedData } = paramsSchema.safeParse(params);
+
+  // params 유효성 검사
+  if (!success) {
+    throw data(
+      { error_code: "invalid_params", message: "Invalid params" },
+      { status: 400 }
+    );
+  }
+  const { year, month, day } = parsedData;
+  const date = DateTime.fromObject({ year, month, day }).setZone("Asia/Seoul");
+  // 날짜 유효성 검사
+  if (!date.isValid) {
+    throw data(
+      { error_code: "invalid_date", message: "Invalid date" },
+      { status: 400 }
+    );
+  }
+  const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+  // 미래 날짜인지 검사
+  if (date > today) {
+    throw data(
+      { error_code: "future_date", message: "Future date" },
+      { status: 400 }
+    );
+  }
+  return { date };
+};
+
+export default function DailyLeaderboard({ loaderData }: Route.ComponentProps) {
   return <div>DailyLeaderboard</div>;
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} / {error.data.error_code}
+      </div>
+    );
+  }
+  if (error instanceof Error) {
+    return <div>{error.message}</div>;
+  }
+  return <div>Unknown Error</div>;
 }
