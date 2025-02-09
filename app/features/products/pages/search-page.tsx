feeb6b707b694375373f0f615e18f4607255d2b5
ohwoo-kwon <kwonow1@gmail.com>
2025-02-09 10:53:08 +0900

    #3.20 Search Page

--- app/features/products/pages/search-page.tsx
+++ app/features/products/pages/search-page.tsx
@@ -1,3 +1,66 @@
-export default function Search() {
-  return <div>Search</div>;
+import { z } from "zod";
+import type { Route } from "./+types/search-page";
+import Hero from "~/common/components/hero";
+import ProductCard from "../components/product-card";
+import ProductPagination from "~/common/components/product-pagination";
+import { Form } from "react-router";
+import { Input } from "~/common/components/ui/input";
+import { Button } from "~/common/components/ui/button";
+
+export const meta: Route.MetaFunction = () => {
+  return [
+    { title: "Search Products | wemake" },
+    { name: "description", content: "Search for products" },
+  ];
+};
+
+const paramsSchema = z.object({
+  query: z.string().optional().default(""),
+  page: z.coerce.number().optional().default(1),
+});
+
+export const loader = ({ request }: Route.LoaderArgs) => {
+  const url = new URL(request.url);
+  const { success, data: parsedData } = paramsSchema.safeParse(
+    Object.fromEntries(url.searchParams)
+  );
+
+  if (!success) {
+    throw new Error("Invalid params");
+  }
+
+  return { parsedData };
+};
+
+export default function Search({ loaderData }: Route.ComponentProps) {
+  return (
+    <div className="space-y-10">
+      <Hero
+        title="Search"
+        subtitle="Search for products by title or description"
+      />
+      <Form className="flex justify-center max-w-screen-sm items-center mx-auto gap-2">
+        <Input
+          name="query"
+          placeholder="Search for products"
+          className="text-lg"
+        />
+        <Button type="submit">Search</Button>
+      </Form>
+      <div className="space-y-5 w-full max-w-screen-md mx-auto">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <ProductCard
+            key={`productId-${idx}`}
+            id={`productId-${idx}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={41}
+            votesCount={120}
+          />
+        ))}
+        <ProductPagination totalPages={10} />
+      </div>
+    </div>
+  );
 }
