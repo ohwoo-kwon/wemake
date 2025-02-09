6982567ab08af603cc03ecafc92184d5ce0ff1f2
ohwoo-kwon <kwonow1@gmail.com>
2025-02-09 11:20:13 +0900

    #3.21 Category Pages

--- app/features/products/pages/categories-page.tsx
+++ app/features/products/pages/categories-page.tsx
@@ -1,3 +1,33 @@
+import Hero from "~/common/components/hero";
+import type { Route } from "./+types/categories-page";
+import CategoryCard from "../components/category-card";
+
+export const meta = () => {
+  return [
+    {
+      title: "Categories | wemake",
+    },
+    { name: "description", content: "Browse products by category" },
+  ];
+};
+
 export default function Categories() {
-  return <div>Categories</div>;
+  return (
+    <div className="space-y-10">
+      <Hero
+        title="Categories"
+        subtitle="Search for products by title or description"
+      />
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-10">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <CategoryCard
+            key={`categoryId-${index}`}
+            id={`categoryId-${index}`}
+            name={`Category Name ${index}`}
+            description="Category Description"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/features/products/pages/category-page.tsx
+++ app/features/products/pages/category-page.tsx
@@ -1,3 +1,36 @@
+import Hero from "~/common/components/hero";
+import type { Route } from "./+types/category-page";
+import ProductCard from "../components/product-card";
+import ProductPagination from "~/common/components/product-pagination";
+
+export const meta: Route.MetaFunction = ({ params }) => {
+  return [
+    { title: `Developer Tools | wemake` },
+    { name: "description", content: `Browse Developer Tools products` },
+  ];
+};
+
 export default function Category() {
-  return <div>Category</div>;
+  return (
+    <div className="space-y-10">
+      <Hero
+        title={`Developer Tools`}
+        subtitle={`Tools for developers to build products faster`}
+      />
+      <div className="space-y-5 w-full max-w-screen-md mx-auto">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <ProductCard
+            key={`productId-${idx}`}
+            id={`productId-${idx}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={41}
+            votesCount={120}
+          />
+        ))}
+        <ProductPagination totalPages={10} />
+      </div>
+    </div>
+  );
 }
