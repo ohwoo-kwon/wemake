e1c9b8698528db723e513b4873f5b17235ea6ed7
ohwoo-kwon <kwonow1@gmail.com>
2025-02-10 21:18:25 +0900

    #3.25 Product Overview Page

---
+++ app/features/products/pages/product-review-page.tsx
@@ -0,0 +1,3 @@
+export default function ProductReview() {
+  return <div>ProductReview</div>;
+}
--- app/routes.ts
+++ app/routes.ts
@@ -39,5 +39,11 @@ export default [
     route("/search", "features/products/pages/search-page.tsx"),
     route("/submit", "features/products/pages/submit-page.tsx"),
     route("/promote", "features/products/pages/promote-page.tsx"),
+    ...prefix("/:productId", [
+      index("features/products/pages/product-redirect-page.tsx"),
+      route("overview", "features/products/pages/product-overview-page.tsx"),
+      route("reviews", "features/products/pages/product-reviews-page.tsx"),
+      route("reviews/new", "features/products/pages/product-review-page.tsx"),
+    ]),
   ]),
 ] satisfies RouteConfig;
