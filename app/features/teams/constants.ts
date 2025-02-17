d8f123850b8490c41bff72e02a0137786ac1843e
ohwoo-kwon <kwonow1@gmail.com>
2025-02-17 20:23:11 +0900

    #4.7 Team Table

---
+++ app/features/teams/constants.ts
@@ -0,0 +1,6 @@
+export const PRODUCT_STAGE = [
+  { label: "Idea", value: "idea" },
+  { label: "Prototype", value: "prototype" },
+  { label: "MVP", value: "mvp" },
+  { label: "Product", value: "product" },
+];
