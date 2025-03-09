32c453c9eda1cfde901e6683c05159c80a9dddc9
ohwoo-kwon <kwonow1@gmail.com>
2025-03-09 21:40:50 +0900

    #7.7 Social Login

--- app/features/auth/pages/social-complete-page.tsx
+++ app/features/auth/pages/social-complete-page.tsx
@@ -1,3 +1,21 @@
-export default function SocialCompletePage() {
-  return <div>SocialCompletePage</div>;
-}
+import { z } from "zod";
+import type { Route } from "./+types/social-complete-page";
+import { redirect } from "react-router";
+import { makeSSRClient } from "~/supa-client";
+
+const paramsSchema = z.object({
+  provider: z.enum(["kakao"]),
+});
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const { success, data } = paramsSchema.safeParse(params);
+  if (!success) return redirect("/auth/login");
+  const { provider } = data;
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+  if (!code) return redirect("/auth/login");
+  const { client, headers } = makeSSRClient(request);
+  const { error } = await client.auth.exchangeCodeForSession(code);
+  if (error) throw error;
+  return redirect("/", { headers });
+};
--- app/features/auth/pages/social-start-page.tsx
+++ app/features/auth/pages/social-start-page.tsx
@@ -1,3 +1,22 @@
-export default function SocialStartPage() {
-  return <div>SocialStartPage</div>;
-}
+import { z } from "zod";
+import type { Route } from "./+types/social-start-page";
+import { redirect } from "react-router";
+import { makeSSRClient } from "~/supa-client";
+
+const paramsSchema = z.object({
+  provider: z.enum(["kakao"]),
+});
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const { success, data } = paramsSchema.safeParse(params);
+  if (!success) return redirect("/auth/login");
+  const { provider } = data;
+  const redirectTo = `http://localhost:3000/auth/social/${provider}/complete`;
+  const { client, headers } = makeSSRClient(request);
+  const {
+    data: { url },
+    error,
+  } = await client.auth.signInWithOAuth({ provider, options: { redirectTo } });
+  if (url) return redirect(url, { headers });
+  if (error) throw error;
+};
